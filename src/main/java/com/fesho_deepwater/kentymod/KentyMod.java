6d1e213aab075897857af4ef4ddb35e8bdb83eb4
Maxim Kudriavtsev <m0505179603@gmail.com>
2021-11-18 21:55:54 +0200

    Start, mods.toml set up

---
+++ src/main/java/com/fesho_deepwater/kentymod/KentyMod.java
@@ -0,0 +1,39 @@
+package com.fesho_deepwater.kentymod;
+
+
+import net.minecraftforge.common.MinecraftForge;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+
+@Mod("kentymod")
+public class KentyMod
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public KentyMod() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+    }
+
+
+
+}
