4e722c6c897c43c42d5f71909597f89a5d318814
DeepWat33r <denis.gidzun@gmail.com>
2021-11-25 22:59:11 +0200

    Add ItemGroup

---
+++ src/main/java/com/fesho_deepwater/kentymod/core/itemgroup/KentyModItemGroup.java
@@ -0,0 +1,19 @@
+package com.fesho_deepwater.kentymod.core.itemgroup;
+
+import com.fesho_deepwater.kentymod.core.init.ItemInit;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+
+public class KentyModItemGroup extends ItemGroup {
+
+    public static final KentyModItemGroup kentymod = new KentyModItemGroup(ItemGroup.GROUPS.length, "kentymod");
+
+    public KentyModItemGroup(int index, String label){
+        super(index, label);
+    }
+
+    @Override
+    public ItemStack createIcon() {
+        return new ItemStack(ItemInit.Silver.get());
+    }
+}
